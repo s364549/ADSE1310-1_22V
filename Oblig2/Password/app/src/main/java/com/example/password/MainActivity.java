bee3291e5aac4e0b11bcd9230a6f97aa3a923f7e
s364549 <s364549@oslomet.no>
2022-03-15 10:03:15 +0100

    Fjernet Kotlin

---
+++ Oblig2/Password/app/src/main/java/com/example/password/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.password;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView browser = (WebView) findViewById(R.id.mywebview);
+        browser.loadUrl("https://pwmgr.herokuapp.com/");
+
+        WebSettings webSettings = browser.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+    }
+}
